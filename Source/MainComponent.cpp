1bea7e4102357ebae77636d7b3578deda72ae268
cha0sIII <94194808+cha0sIII@users.noreply.github.com>
2022-02-02 13:09:42 -0500

    New toggle

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,6 +3,7 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
+    addAndMakeVisible(toggleButton);
     setSize (600, 400);
 }
 
@@ -29,4 +30,5 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+    toggleButton.setBounds(10, 10, 30, 30);
 }
